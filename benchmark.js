71f17c33a524a471ecbbd27e59485ab1b1e8e2b2
Andras <andras@kinvey.com>
2017-03-06 13:13:35 -0500

    replaced benchmark comparisons with qtimeit results, removed old timings; 0.11.0

--- benchmark.js
+++ benchmark.js
@@ -1,5 +1,7 @@
 // qprintf benchmark, see git://github.com/andrasq/node-qprintf
 
+var version = require('./package.json').version;
+
 var util = require('util');
 var qtimeit = require('qtimeit');
 
@@ -10,6 +12,12 @@ var sprintf = require('sprintf').sprintf;
 var printf = require('printf');
 var extsprintf = require('extsprintf').sprintf;
 
+var x;
+qtimeit.bench.timeGoal = 0.4;
+qtimeit.bench([
+    function(){ z = qsprintf("%s %04d %s", "Hello", 123, "world") }
+]);
+
 var fmt1 = "%s %d %s";
 var fmt2 = "%s %04d %s %4$5.2f";
 for (var loop=0; loop<10; loop++) {
@@ -19,12 +27,12 @@ for (var loop=0; loop<10; loop++) {
         'sprintfjs-1.0.3': sprintfjs,
         'extsprintf-1.3.0': extsprintf,         // does not handle floating-point or %4$
         'sprintf-0.1.5': sprintf,
-        'qprintf-0.9.2': qsprintf,
+        'qprintf-0.11.0': qsprintf,
 //        'util_format': util.format,             // does not handle %04 or %4$
     };
 
     for (var f=0; f<2; f++) {
-    for (var nloops=0; nloops<5; nloops++) {
+    for (var nloops=0; nloops<3; nloops++) {
         for (var callName in calls) {
             var call = calls[callName];
             var t1 = Date.now();
@@ -44,7 +52,7 @@ for (var loop=0; loop<10; loop++) {
         'sprintfjs-1.0.3': function(){ z = sprintfjs(fmt1, "Hello", 123, "world") },
         'extsprintf-1.3.0': function(){ z = extsprintf(fmt1, "Hello", 123, "world") },
         'sprintf-0.1.5': function(){ z = sprintf(fmt1, "Hello", 123, "world") },
-        'qprintf-0.9.2': function(){ z = qsprintf(fmt1, "Hello", 123, "world") },
+        'qprintf-0.11.0': function(){ z = qsprintf(fmt1, "Hello", 123, "world") },
         //'util_format': function(){ util.format(fmt1, "Hello", 123, "world") },
     };
     var bench2 = {
@@ -52,7 +60,7 @@ for (var loop=0; loop<10; loop++) {
         'sprintfjs-1.0.3': function(){ z = sprintfjs(fmt2, "Hello", 123, "world", 12.345) },
         'extsprintf-1.3.0': function(){ z = extsprintf(fmt2, "Hello", 123, "world", 12.345) },
         'sprintf-0.1.5': function(){ z = sprintf(fmt2, "Hello", 123, "world", 12.345) },
-        'qprintf-0.9.2': function(){ z = qsprintf(fmt2, "Hello", 123, "world", 12.345) },
+        'qprintf-0.11.0': function(){ z = qsprintf(fmt2, "Hello", 123, "world", 12.345) },
         //'util_format': function(){ util.format(fmt2, "Hello", 123, "world", 12.345) },
     };
 
